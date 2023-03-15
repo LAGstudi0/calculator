b7ef5024ad601c6dc51061d29bcf5224b10f89d5
LAGstudi0 <paolo.milutinovic@gmail.com>
2023-03-15 17:36:24 +0000

    Major changes made in the style colour palette

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function resetCalculus()    {
     calculus.typeOfOperand = null;
     calculus.result = null;
 }
-
+// doesnt work
 function clear()    {
     resetCalculus();
     changeDisplay("");
